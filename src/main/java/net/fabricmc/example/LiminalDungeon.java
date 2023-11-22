3aaead4a663143f03bf571486a926ffd2b32c5be
B80B1 <tjanders.62@gmail.com>
2023-11-22 00:14:56 -0600

    E

---
+++ src/main/java/net/fabricmc/example/LiminalDungeon.java
@@ -0,0 +1,11 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LiminalDungeon implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
